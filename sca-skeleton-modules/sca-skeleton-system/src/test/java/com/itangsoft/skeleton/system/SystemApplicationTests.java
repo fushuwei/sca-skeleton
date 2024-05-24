6fcccc2b74e1aaa2559a6a2eab0722ddc2d61534
fushuwei <331044761@qq.com>
2024-05-24 15:11:48 +0800

    创建 sca-skeleton-system 模块

---
+++ sca-skeleton-modules/sca-skeleton-system/src/test/java/com/itangsoft/skeleton/system/SystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itangsoft.skeleton.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
