bc3ba843f0ebccf3bad739c46e18b92592b3a6cd
fushuwei <331044761@qq.com>
2026-05-16 13:35:22 +0800

    提交修改

---
+++ sca-skeleton-backend/sca-skeleton-starters/sca-skeleton-starter-security/src/main/java/io/github/fushuwei/sca/starter/security/annotation/RequiresPermission.java
@@ -0,0 +1,23 @@
+package io.github.fushuwei.sca.starter.security.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 权限校验注解
+ *
+ * @author Fu Wei
+ */
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface RequiresPermission {
+
+    /**
+     * 权限标识
+     */
+    String value();
+}
