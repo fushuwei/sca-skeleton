5958e49d3c7f4603547e04b03fd65579f188d338
fushuwei <331044761@qq.com>
2026-06-03 22:41:24 +0800

    提交修改

--- sca-skeleton-backend/sca-skeleton-auth/src/main/java/io/github/fushuwei/scaskeleton/auth/AuthApplication.java
+++ sca-skeleton-backend/sca-skeleton-auth/src/main/java/io/github/fushuwei/scaskeleton/auth/AuthApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * 认证服务启动类。
@@ -14,6 +15,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @author Fu Wei
  */
 @MapperScan("io.github.fushuwei.scaskeleton.auth.infrastructure.mapper")
+@EnableRedisHttpSession
 @EnableDiscoveryClient
 @SpringBootApplication
 public class AuthApplication {
