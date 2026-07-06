99dcdbaa26c8e2ba20f0e1442a21ccae038bea9b
fushuwei <331044761@qq.com>
2026-07-06 13:33:56 +0800

    提交修改

--- sca-skeleton-frontend/apps/admin/src/views/system/UserDrawerContent.vue
+++ sca-skeleton-frontend/apps/admin/src/views/system/UserDrawerContent.vue
@@ -8,7 +8,7 @@ import { getDeptListApi } from "../../apis/dept";
 import { getPostListApi } from "../../apis/post";
 import { getRoleListApi } from "../../apis/role";
 import { checkPasswordStrength } from "../../utils/passwordStrength";
-import DateTimePicker from "../../components/DateTimePicker.vue";
+
 
 const { t } = useI18n({ useScope: "global" });
 
@@ -732,20 +732,26 @@ async function handleSave() {
         </div>
         <!-- 生效时间 -->
         <div class="col-12 col-md-6">
-          <DateTimePicker
+          <q-input
             v-model="form.effectiveStartTime"
             :label="t('user.effectiveStartTime')"
+            filled
+            square
             :disable="drawerReadonly"
             :readonly="drawerReadonly"
+            hide-bottom-space
           />
         </div>
         <!-- 失效时间 -->
         <div class="col-12 col-md-6">
-          <DateTimePicker
+          <q-input
             v-model="form.effectiveEndTime"
             :label="t('user.effectiveEndTime')"
+            filled
+            square
             :disable="drawerReadonly"
             :readonly="drawerReadonly"
+            hide-bottom-space
           />
         </div>
         <!-- 备注 -->
