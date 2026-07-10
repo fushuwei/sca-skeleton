d784bc39abdf579188c7075a5806b76bdafb9610
fushuwei <331044761@qq.com>
2026-07-10 16:15:36 +0800

    提交修改

--- deploy/sql/install/mock/truncate_all.sql
+++ deploy/sql/install/mock/truncate_all.sql
@@ -7,40 +7,40 @@
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- 清空用户与权限相关表
-TRUNCATE TABLE `sys_user_role`;
-TRUNCATE TABLE `sys_role_permission`;
-TRUNCATE TABLE `sys_role_data_scope`;
-TRUNCATE TABLE `sys_user_dept`;
-TRUNCATE TABLE `sys_user_post`;
-TRUNCATE TABLE `sys_user_password_history`;
-TRUNCATE TABLE `sys_permission`;
-TRUNCATE TABLE `sys_role`;
-TRUNCATE TABLE `sys_user`;
+DELETE FROM `sys_user_role`;
+DELETE FROM `sys_role_permission`;
+DELETE FROM `sys_role_data_scope`;
+DELETE FROM `sys_user_dept`;
+DELETE FROM `sys_user_post`;
+DELETE FROM `sys_user_password_history`;
+DELETE FROM `sys_permission`;
+DELETE FROM `sys_role`;
+DELETE FROM `sys_user`;
 
 -- 清空组织架构相关表
-TRUNCATE TABLE `sys_dept`;
-TRUNCATE TABLE `sys_post`;
+DELETE FROM `sys_dept`;
+DELETE FROM `sys_post`;
 
 -- 清空租户相关表
-TRUNCATE TABLE `sys_tenant_package_permission`;
-TRUNCATE TABLE `sys_tenant_package`;
-TRUNCATE TABLE `sys_tenant`;
+DELETE FROM `sys_tenant_package_permission`;
+DELETE FROM `sys_tenant_package`;
+DELETE FROM `sys_tenant`;
 
 -- 清空系统配置相关表
-TRUNCATE TABLE `sys_recycle_bin`;
-TRUNCATE TABLE `sys_dict_data`;
-TRUNCATE TABLE `sys_dict`;
-TRUNCATE TABLE `sys_config`;
-TRUNCATE TABLE `sys_release`;
+DELETE FROM `sys_recycle_bin`;
+DELETE FROM `sys_dict_data`;
+DELETE FROM `sys_dict`;
+DELETE FROM `sys_config`;
+DELETE FROM `sys_release`;
 
 -- 清空日志表
-TRUNCATE TABLE `sys_login_log`;
-TRUNCATE TABLE `sys_operation_log`;
+DELETE FROM `sys_login_log`;
+DELETE FROM `sys_operation_log`;
 
 -- 清空 OAuth2 相关表
-TRUNCATE TABLE `oauth2_authorization_consent`;
-TRUNCATE TABLE `oauth2_authorization`;
-TRUNCATE TABLE `oauth2_registered_client`;
+DELETE FROM `oauth2_authorization_consent`;
+DELETE FROM `oauth2_authorization`;
+DELETE FROM `oauth2_registered_client`;
 
 -- 打开外键检查
 SET FOREIGN_KEY_CHECKS = 1;
