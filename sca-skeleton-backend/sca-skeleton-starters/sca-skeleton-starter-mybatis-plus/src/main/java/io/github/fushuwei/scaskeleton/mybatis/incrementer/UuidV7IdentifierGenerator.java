e0d240f474b4e0fbd8adea1118a3b153a0d3ed63
fushuwei <331044761@qq.com>
2026-07-15 13:27:44 +0800

    提交修改

--- sca-skeleton-backend/sca-skeleton-starters/sca-skeleton-starter-mybatis-plus/src/main/java/io/github/fushuwei/scaskeleton/mybatis/incrementer/UuidV7IdentifierGenerator.java
+++ sca-skeleton-backend/sca-skeleton-starters/sca-skeleton-starter-mybatis-plus/src/main/java/io/github/fushuwei/scaskeleton/mybatis/incrementer/UuidV7IdentifierGenerator.java
@@ -4,10 +4,7 @@ import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
 import io.github.fushuwei.scaskeleton.core.uuid.UuidUtils;
 
 /**
- * 基于 UUID v7 的主键生成器，替代 MyBatis-Plus 默认的随机 UUID。
- * <p>
- * 配合 {@code @TableId(type = IdType.ASSIGN_UUID)} 使用，INSERT 时由框架调用
- * {@link #nextUUID(Object)} 生成 32 位无连字符的 UUID v7（时间排序，数据库主键首选）。
+ * 基于 UUID v7 的主键生成器，替代 MyBatis-Plus 默认的随机 UUID
  *
  * @author Fu Wei
  */
