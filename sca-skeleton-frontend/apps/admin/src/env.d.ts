02c644e119e8b25be216ef4390d4b7ad64d6eeed
fushuwei <331044761@qq.com>
2026-07-24 14:51:56 +0800

    提交修改

--- sca-skeleton-frontend/apps/admin/src/env.d.ts
+++ sca-skeleton-frontend/apps/admin/src/env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  readonly VITE_BASE_URL: string;
   readonly VITE_API_BASE_URL: string;
   readonly VITE_OAUTH_CLIENT_ID: string;
   readonly VITE_OAUTH_REDIRECT_URI: string;
