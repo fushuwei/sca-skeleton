eec3f7affb5f8b8aaa266e2af20f75dd37f8d951
fushuwei <331044761@qq.com>
2026-07-24 23:12:59 +0800

    提交修改

---
+++ deploy/app/system.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /data/projects/sca-skeleton || exit 1
+JAR="./bin/sca-skeleton-system-biz-1.0.0.jar"
+pkill -9 -f "$JAR"
+sleep 1
+set -a
+source ./bin/.env
+set +a
+nohup java -jar "$JAR" &
