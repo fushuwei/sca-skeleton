4a8374f4d8657e005b3de33a858f904a77514e11
fushuwei <331044761@qq.com>
2026-08-17 20:24:56 +0800

    提交修改

--- deploy/sql/install/mock/sample_data.sql
+++ deploy/sql/install/mock/sample_data.sql
@@ -11,7 +11,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 INSERT INTO sys_tenant_package (id,name,code,status,user_limit,api_limit,storage_limit,expire_days,sort,remark,version,create_by,create_time,update_by,update_time,is_deleted) VALUES
 	 ('2','基础套餐','base','enabled',20,1000,10,180,100,NULL,1,'1',now(),'1',now(),0),
 	 ('3','迷你套餐','mini','enabled',10,500,5,180,100,NULL,0,'1',now(),'1',now(),0),
-	 ('4','测试套餐（已停用）','test','disabled',-1,-1,-1,-1,100,NULL,0,'1',now(),'1',now(),0);
+	 ('4','临时套餐','temp','disabled',-1,-1,-1,-1,100,NULL,0,'1',now(),'1',now(),0);
 
 
 -- ================================================================
